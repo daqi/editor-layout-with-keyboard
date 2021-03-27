5da942e8388964d16a1a539d9922cd8ff34b4163
daqi <daqidaqi@qq.com>
2021-03-28 01:34:49 +0800

    demo publicPath, readme add link and qrcode

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,12 @@
 const CracoLessPlugin = require('craco-less');
 
 module.exports = {
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      webpackConfig.output.publicPath = '/editor-layout-with-keyboard/';
+      return webpackConfig;
+    },
+  },
   plugins: [
     {
       plugin: CracoLessPlugin,
